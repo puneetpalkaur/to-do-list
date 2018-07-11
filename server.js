98ade104e8e6f9d0a21e0bcecb0392b3212a2134
puneetpalkaur <billingpuneet.13@gmail.com>
2018-07-11 02:56:50 -0400

    express works , voila

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/to-do-list')));
 
 
 
