3180d89a4ad3221e60120fad107566ae37c2d202
puneetpalkaur <billingpuneet.13@gmail.com>
2018-07-11 02:41:24 -0400

    updated package json for version

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+// Send all other requests to the Angular app
+app.get('/*', function (req, res){
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, function () {console.log('Running on localhost')});
